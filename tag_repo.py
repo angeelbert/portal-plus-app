d78ad1be98e7f7dbe3d75052e9f52f40d74dc830
angeelbert <angeelbert@hotmail.com>
2024-05-23 16:32:41 -0400

    GITHUB_TOKEN 3

--- tag_repo.py
+++ tag_repo.py
@@ -13,20 +13,20 @@ def increment_version(version, version_type):
         parts[2] = '0'
     elif version_type == 'patch':
         parts[2] = str(int(parts[2]) + 1)
-    return 'v' + '.'.join(parts)  # Corregido aquí
+    return 'v' + '.'.join(parts)
 
 repo = git.Repo('.')
 try:
     current_version = repo.git.describe(tags=True, abbrev=0)
 except git.exc.GitCommandError:
-    current_version = 'v0.1.0'  # Establecer una versión inicial si no hay tags previos
+    current_version = 'v0.1.0'  # Set an initial version if no tags exist
 
 version_type = sys.argv[1]
 new_version = increment_version(current_version, version_type)
 
 repo.create_tag(new_version)
 
-# Configurar autenticación utilizando el token
+# Configure authentication using the token
 origin = repo.remote(name='origin')
 origin.set_url(f'https://{os.getenv("GITHUB_TOKEN")}@github.com/{os.getenv("GITHUB_REPOSITORY")}.git')
 origin.push('refs/tags/' + new_version)
