76eafcd98c94cbdcea1580b4084dd85f1e860ff4
angeelbert <angeelbert@hotmail.com>
2024-05-24 19:46:59 -0400

    test2

--- tag_and_release.sh
+++ tag_and_release.sh
@@ -5,16 +5,16 @@
 while IFS= read -r repo_url; do
   echo "Tagging and releasing in $repo_url"
   repo_name=$(basename -s .git "$repo_url")
-  authenticated_repo_url="https://${GH_TOKEN}@${repo_url}"
+  authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
   git clone "$authenticated_repo_url"
-  cd "$repo_name" || exit
+  cd "$repo_name"
   git config user.name "github-actions[bot]"
   git config user.email "github-actions[bot]@users.noreply.github.com"
   git tag "$1"
   git push "$authenticated_repo_url" --tags
-  repo_api_url="https://api.github.com/repos/${repo_url}/releases"
+  repo_api_url="https://api.github.com/repos/${repo_url#https://github.com/}/releases"
   release_response=$(curl -X POST \
-    -H "Authorization: token ${GH_TOKEN}" \
+    -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "$repo_api_url" \
     -d @- <<EOF
@@ -22,7 +22,7 @@ while IFS= read -r repo_url; do
   "tag_name": "$1",
   "target_commitish": "main",
   "name": "Release $1",
-  "body": "### [$1](https://github.com/${repo_url}/compare/v${1}...v${1}) (2024-05-24)",
+  "body": "### [$1](https://github.com/${{ github.repository }}/compare/v${{ steps.tag_version.outputs.previous_tag }}...v$1) (2024-05-24)",
   "draft": false,
   "prerelease": false
 }
