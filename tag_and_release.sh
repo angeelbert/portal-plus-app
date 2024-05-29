78a76901aec542dce4ae6571ca0a84ad6fd173b6
angeelbert <angeelbert@hotmail.com>
2024-05-29 19:13:16 -0400

    test new script

--- tag_and_release.sh
+++ tag_and_release.sh
@@ -1,64 +1,32 @@
 #!/bin/bash
 
-# Function to tag a specific file
-tag_file() {
-  local file=$1
-  local tag=$2
-  echo "Tagging $file with $tag"
-  echo "File: $file" >> FILE_TAGS.md
-  echo "Tags: $tag" >> FILE_TAGS.md
-  echo "" >> FILE_TAGS.md
-}
+# tag_and_release.sh
 
-while IFS= read -r repo_url; do
-  echo "Processing repository $repo_url"
+# Función para etiquetar y lanzar un solo repositorio
+tag_and_release_repo() {
+  repo_url="$1"
+  tag_name="$2"
+
+  echo "Tagging and releasing in $repo_url"
   repo_name=$(basename -s .git "$repo_url")
   authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
   git clone "$authenticated_repo_url"
   cd "$repo_name"
-  
-  if [[ ! -f files.txt ]]; then
-    echo "Error: files.txt not found in the script directory."
-    cd ..
-    rm -rf "$repo_name"
-    continue
-  fi
-  
-  # Ensure the tag file exists
-  touch FILE_TAGS.md
-
-  # Set up Git configuration
   git config user.name "github-actions[bot]"
   git config user.email "github-actions[bot]@users.noreply.github.com"
-  
-  # Iterate over files to tag
-  while IFS= read -r file; do
-    if [[ -f $file ]]; then
-      tag_file "$file" "$1"
-    #   git add ./$file
-    else
-      echo "Warning: $file does not exist in $repo_name"
-    fi
-  done < files.txt
-
-  # Commit and push changes
-  git add FILE_TAGS.md
-  git commit -m "Tagging files with $1"
-  git push "$authenticated_repo_url" main
-
-  # Create a release
+  git tag "$tag_name"
+  git push "$authenticated_repo_url" --tags
   repo_api_url="https://api.github.com/repos/${repo_url#https://github.com/}/releases"
-  echo "Creating release at URL: $repo_api_url"
   release_response=$(curl -X POST \
     -H "Authorization: token $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "$repo_api_url" \
     -d @- <<EOF
 {
-  "tag_name": "$1",
+  "tag_name": "$tag_name",
   "target_commitish": "main",
-  "name": "Release $1",
-  "body": "### Release $1 (2024-05-24)",
+  "name": "Release $tag_name",
+  "body": "### [$tag_name](https://github.com/${{ github.repository }}/compare/v${{ steps.tag_version.outputs.previous_tag }}...v$tag_name) (2024-05-24)",
   "draft": false,
   "prerelease": false
 }
@@ -70,5 +38,18 @@ EOF
     echo "Failed to create release for $repo_url: $release_response"
   fi
   cd ..
-  rm -rf "$repo_name"
+}
+
+# Etiquetar y lanzar repositorios
+while IFS= read -r repo_url; do
+  tag_and_release_repo "$repo_url" "$1"
 done < repos.txt
+
+# Etiquetar archivos en las rutas proporcionadas
+while IFS= read -r file_path; do
+  echo "Tagging file: $file_path"
+  tag_name=$(basename "$file_path")
+  git tag "$tag_name" --force # Etiquetar el archivo
+  git push --tags # Empujar la etiqueta al repositorio remoto
+  echo "Tagged file $file_path with tag $tag_name"
+done < files.txt
