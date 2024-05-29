2dfae795c4f12090a4ac9a22829c5e1764a47be4
angeelbert <angeelbert@hotmail.com>
2024-05-29 19:38:54 -0400

    test17

--- tag_and_release.sh
+++ tag_and_release.sh
@@ -8,26 +8,25 @@ tag_and_release_repo() {
   tag_name="$2"
 
   echo "Tagging and releasing in $repo_url"
-  repo_name=$(basename "$(echo "$repo_url" | sed 's/\.git$//')" | tr -d '\r')  # Elimina el retorno de carro (\r) si está presente en el nombre del repositorio
+  repo_name=$(basename "$(echo "$repo_url" | sed 's/\.git$//')" | tr -d '\r')
+  authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
+  
   if git ls-remote --tags "$repo_url" | grep -q "refs/tags/$tag_name"; then
     echo "Tag $tag_name already exists in $repo_url. Skipping tagging."
-    return 0
-  fi
-  if [ -d "$repo_name" ]; then
-    echo "Repository directory already exists. Skipping cloning."
   else
-    authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
-    git clone "$authenticated_repo_url" "$repo_name" || { echo "Failed to clone repository: $repo_url"; exit 1; }
-  fi
-  cd "$repo_name" || { echo "Failed to change directory to repository: $repo_name"; exit 1; }
-  git config user.name "github-actions[bot]"
-  git config user.email "github-actions[bot]@users.noreply.github.com"
-  if git rev-parse --verify --quiet "refs/tags/$tag_name"; then
-    echo "Tag $tag_name already exists. Skipping tagging."
-  else
+    if [ -d "$repo_name" ]; then
+      echo "Repository directory already exists. Skipping cloning."
+    else
+      git clone "$authenticated_repo_url" "$repo_name" || { echo "Failed to clone repository: $repo_url"; exit 1; }
+    fi
+    cd "$repo_name" || { echo "Failed to change directory to repository: $repo_name"; exit 1; }
+    git config user.name "github-actions[bot]"
+    git config user.email "github-actions[bot]@users.noreply.github.com"
     git tag "$tag_name"
     git push "$authenticated_repo_url" --tags || { echo "Failed to push tags to repository: $repo_url"; exit 1; }
+    cd ..
   fi
+
   repo_api_url="https://api.github.com/repos/${repo_url#https://github.com/}/releases"
   release_response=$(curl -X POST \
     -H "Authorization: token $GH_TOKEN" \
@@ -49,7 +48,6 @@ EOF
   else
     echo "Failed to create release for $repo_url: $release_response"
   fi
-  cd ..
 }
 
 # Etiquetar y lanzar repositorios
@@ -59,11 +57,12 @@ done < repos.txt
 
 # Etiquetar archivos en las rutas proporcionadas
 if [ -f "files.txt" ]; then
+  authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
   while IFS= read -r file_path; do
     echo "Tagging file: $file_path"
-    tag_name=$(basename "$file_path")
+    tag_name=$(basename "$file_path" | tr -d '\r')
     git tag "$tag_name" --force # Etiquetar el archivo
-    git push --tags # Empujar la etiqueta al repositorio remoto
+    git push "$authenticated_repo_url" --tags # Empujar la etiqueta al repositorio remoto
     echo "Tagged file $file_path with tag $tag_name"
   done < files.txt
 else
