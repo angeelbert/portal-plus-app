16328197ffd7c3c376042dd6be17b809d3b98aed
angeelbert <angeelbert@hotmail.com>
2024-05-29 20:27:50 -0400

    test34

--- tag_and_release.sh
+++ tag_and_release.sh
@@ -2,59 +2,60 @@
 
 # tag_and_release.sh
 
-# Función para agregar tag a un archivo
-tag_and_commit_file() {
-  file_path="$1"
-  tag_name="$2"
-
-  # Obtener el nombre del archivo sin la extensión
-  file_name=$(basename "$file_path" | sed 's/\.[^.]*$//')
-
-  echo "Tagging file: $file_path with tag $tag_name"
-
-  # Clonar el repositorio si no está clonado
-  if [ ! -d "$file_name" ]; then
-    git clone "$authenticated_repo_url" "$file_name" || { echo "Failed to clone repository: $authenticated_repo_url"; exit 1; }
-  fi
-
-  cd "$file_name" || { echo "Failed to change directory to repository: $file_name"; exit 1; }
-
-  git config user.name "github-actions[bot]"
-  git config user.email "github-actions[bot]@users.noreply.github.com"
-
-  # Incrementar el tag si ya existe
-  while git tag | grep -q "^$tag_name$"; do
-    echo "Tag $tag_name already exists for file $file_path. Incrementing tag version."
-    tag_name=$(echo "$tag_name" | awk -F '.' '{$NF = $NF + 1;} 1' OFS='.')
-  done
-
-  # Agregar el tag al archivo y hacer el commit
-  git tag "$tag_name" "$file_path"
-  git commit -m "Tagging file $file_path with version $tag_name"
-  git push origin "$tag_name" || { echo "Failed to push tag $tag_name for file $file_path"; exit 1; }
-
-  echo "Committed and pushed changes for file $file_path with tag $tag_name"
-
-  cd ..
+# Function to tag files
+tag_files() {
+  local repo_url="$1"
+  local tag="$2"
+  
+  while IFS= read -r file_path; do
+    echo "Tagging file: $file_path"
+    authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
+    cd "$repo_name"
+    git config user.name "github-actions[bot]"
+    git config user.email "github-actions[bot]@users.noreply.github.com"
+    
+    # Check if the file exists
+    if [ -f "$file_path" ]; then
+      # Increment tag version if tag already exists
+      latest_tag=$(git tag --list "$tag" | tail -n 1)
+      if [ "$latest_tag" = "$tag" ]; then
+        tag_number=$(echo "$tag" | cut -d '.' -f 2)
+        new_tag="v0.0.$((tag_number + 1))"
+        git tag "$new_tag" "$file_path"
+        git push "$authenticated_repo_url" --tags
+      else
+        git tag "$tag" "$file_path"
+        git push "$authenticated_repo_url" --tags
+      fi
+    else
+      echo "File $file_path does not exist."
+    fi
+    
+    cd ..
+  done < files.txt
 }
 
-# Etiquetar y lanzar repositorios desde repos.txt
 while IFS= read -r repo_url; do
   echo "Tagging and releasing in $repo_url"
   repo_name=$(basename -s .git "$repo_url")
   authenticated_repo_url="https://${GH_TOKEN}@${repo_url#https://}"
-  git clone "$authenticated_repo_url" || { echo "Failed to clone repository: $repo_url"; exit 1; }
-  repo_name=$(echo "$repo_name" | tr -d '\r') # Eliminar el retorno de carro
-  cd "$repo_name" || { echo "Failed to change directory to repository: $repo_name"; exit 1; }
+  git clone "$authenticated_repo_url"
+  cd "$repo_name"
   git config user.name "github-actions[bot]"
   git config user.email "github-actions[bot]@users.noreply.github.com"
-  # Incrementar el tag si ya existe
-  while git tag | grep -q "^$1$"; do
-    echo "Tag $1 already exists for repository $repo_url. Incrementing tag version."
-    set -- "$(echo "$1" | awk -F '.' '{$NF = $NF + 1;} 1' OFS='.')"
-  done
-  git tag "$1" || { echo "Failed to tag $1 for repository $repo_url"; exit 1; }
-  git push "$authenticated_repo_url" --tags || { echo "Failed to push tags to repository: $authenticated_repo_url"; exit 1; }
+  
+  # Increment tag version if tag already exists
+  latest_tag=$(git tag --list "$1" | tail -n 1)
+  if [ "$latest_tag" = "$1" ]; then
+    tag_number=$(echo "$1" | cut -d '.' -f 2)
+    new_tag="v0.0.$((tag_number + 1))"
+    git tag "$new_tag"
+    git push "$authenticated_repo_url" --tags
+  else
+    git tag "$1"
+    git push "$authenticated_repo_url" --tags
+  fi
+  
   repo_api_url="https://api.github.com/repos/${repo_url#https://github.com/}/releases"
   release_response=$(curl -X POST \
     -H "Authorization: token $GH_TOKEN" \
@@ -65,7 +66,7 @@ while IFS= read -r repo_url; do
   "tag_name": "$1",
   "target_commitish": "main",
   "name": "Release $1",
-  "body": "### [$1]($authenticated_repo_url/compare/v${1}...v$1) (2024-05-24)",
+  "body": "### [$1]($authenticated_repo_url/compare/$latest_tag...$1) (2024-05-24)",
   "draft": false,
   "prerelease": false
 }
@@ -76,14 +77,13 @@ EOF
   else
     echo "Failed to create release for $repo_url: $release_response"
   fi
+  
+  # Tag files
+  if [ -f "files.txt" ]; then
+    tag_files "$repo_url" "$1"
+  else
+    echo "No files.txt found. Skipping file tagging."
+  fi
+  
   cd ..
 done < repos.txt
-
-# Etiquetar archivos en las rutas proporcionadas en files.txt
-if [ -f "files.txt" ]; then
-  while IFS= read -r file_path; do
-    tag_and_commit_file "$file_path" "$1"
-  done < files.txt
-else
-  echo "No files.txt found. Skipping file tagging."
-fi
